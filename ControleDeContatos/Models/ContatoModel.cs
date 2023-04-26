f8acad1e16c0b3681786900dddef30e61ca58566
Jackson Ricardo Wippel <jrwippel@hotmail.com>
2023-04-26 11:55:53 -0300

    Created validation of mandatory fields on the screen

--- ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -1,10 +1,19 @@
-﻿namespace ControleDeContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
 {
     public class ContatoModel
-    {
+    {        
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome do contato é obrigatório")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Email do contato é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email do contato é obrigatório")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Telefone do contato é obrigatório")]
+        [Phone(ErrorMessage = "Telefone do contato Inválido")]
         public string Telefone { get; set; }
     }
 }
