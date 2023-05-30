634e63830f7735de1de69fa37dff5d4a714672f7
Jackson Ricardo Wippel <jrwippel@hotmail.com>
2023-05-30 16:33:54 -0300

    Created routine user profile

---
+++ ControleDeContatos/Filters/PaginaRestritaSomenteAdmin.cs
@@ -0,0 +1,34 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+
+namespace ControleDeContatos.Filters
+{
+    public class PaginaRestritaSomenteAdmin : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            string sessaoUsuario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "login" }, { "action", "index" } });
+            }
+            else
+            {
+                UsuarioModel usuario = JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);
+                if (usuario == null) 
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "login" }, { "action", "index" } });
+                }
+                if (usuario.Perfil != Enums.PerfilEnum.Admin)
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "restrito" }, { "action", "index" } });
+
+                }
+            }
+            base.OnActionExecuted(context);
+        }     
+    }
+}
